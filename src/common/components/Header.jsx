404d2e355339eb36ec43f59f1377dc7426fb511f
Olivia Zhao <oxzhao24@gmail.com>
2025-03-08 17:31:03 -0600

    created a header component, more work on general-info page

---
+++ src/common/components/Header.jsx
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react';
+
+import styled from 'styled-components';
+
+import { UserContext } from 'common/contexts/UserContext';
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  padding: 20px;
+`;
+
+const Names = styled.div`
+  font-size: 25px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  font-weight: bold;
+`;
+
+const Dates = styled.div`
+  font-weight: normal;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: right;
+  height: 100%;
+  font-size: 12px;
+`;
+
+export default function Header() {
+  const { user } = useContext(UserContext);
+
+  return (
+    <HeaderContainer>
+      <Names>
+        {user?.firstname || 'User'} {user?.lastname || 'User'}
+      </Names>
+      <Dates>
+        <p>Date Created: {user?.created_at}</p>
+        <p>Date Updated: {user?.updated_at}</p>
+      </Dates>
+    </HeaderContainer>
+  );
+}
--- src/pages/general-info/GeneralInfo.jsx
+++ src/pages/general-info/GeneralInfo.jsx
@@ -1,37 +1,79 @@
 import React, { useContext } from 'react';
 
+// import { useEffect, useState } from 'react';
+
+/* import { createClient } from '@supabase/supabase-js';
+import { useParams } from 'react-router-dom'; */
 import styled from 'styled-components';
 
-import { Subtitle } from 'common/components/Text';
-import UsersList from 'common/components/Users/UsersList';
+import Header from 'common/components/Header';
 import { UserContext } from 'common/contexts/UserContext';
 
-const TextContainer = styled.div`
+/* const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+); */
+
+const Table = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 10px;
+  align-items: left;
+  font-size: 13px;
 `;
 
 const InfoPage = styled.div`
   flex: 1 0 0;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  justify-content: left;
+  align-items: left;
+  text-align: left;
   padding: 2rem;
 `;
 
 export default function GeneralInfo() {
+  /* const { id } = useParams(); // Get participant ID from URL
+  const [participant, setParticipant] = useState(null);
+
+  useEffect(() => {
+    const fetchParticipant = async () => {
+      const { data, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', id);
+
+      if (error) {
+        console.error('Error fetching participants:', error);
+      } else {
+        setParticipant(data);
+      }
+    };
+    fetchParticipant();
+  }, []); */
   const { user } = useContext(UserContext);
 
   return (
     <InfoPage>
-      <TextContainer>
-        <Subtitle>
-          Hello, {user?.firstname || 'User'} {user?.lastname || 'User'}!
-        </Subtitle>
-      </TextContainer>
-      <UsersList />
+      <Header />
+      <section>
+        Constituent Info:
+        <Table>
+          <p>Last Name: {user?.last_name}</p>
+          <p>First Name: {user?.first_name}</p>
+          <p>Middle Name: {user?.middle_name}</p>
+          <p>Nickname: {user?.nickname}</p>
+          <p>SSN: *********</p>
+          <p>Gender: {user?.gender}</p>
+          <p>Date of Birth: {user?.date_of_birth}</p>
+          <p>Suffix: {user?.suffix}</p>
+          <p>Maiden Name: {user?.maiden_name}</p>
+          <p>Suffix: {user?.suffix}</p>
+          <p>Contract Code: {user?.contract_code}</p>
+          <p>Caregiver/Recipient: {user?.care_giver}</p>
+          <p>Preferred Worker: {user?.preferred_worker}</p>
+          <p>Status: {user?.status}</p>
+        </Table>
+      </section>
     </InfoPage>
   );
 }
