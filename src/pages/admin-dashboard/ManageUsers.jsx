b3652f6f1d8d45f84af7096f3eca3e245e13d4f0
Aria Shi <ariashi15@gmail.com>
2025-05-27 11:47:38 -0500

    added api call for users

--- src/pages/admin-dashboard/ManageUsers.jsx
+++ src/pages/admin-dashboard/ManageUsers.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import {
   Table,
@@ -11,6 +11,46 @@ import {
 import { InlayContainer } from './styles';
 
 export default function ManageUsers() {
+  const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [noDataReason, setNoDataReason] = useState('');
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setIsLoading(true);
+      try {
+        const baseUrl = process.env.REACT_APP_BACKEND_URL;
+        console.log(`Fetching participants from ${baseUrl}/users`);
+        const response = await fetch(`${baseUrl}/users`, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include', // Send cookies if authentication is needed
+        });
+        if (!response.ok) {
+          throw new Error(`API error: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (data.length === 0) {
+          setNoDataReason('No participants found in database');
+        }
+        setError(null);
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setError(`Failed to load users: ${error.message}`);
+        setUsers([]);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <InlayContainer>
       <div className='text-subheading'>Registered Users</div>
@@ -23,18 +63,15 @@ export default function ManageUsers() {
             <TableHeaderCell>Role</TableHeaderCell>
             <TableHeaderCell>Email</TableHeaderCell>
           </TableRow>
-          <TableRow>
-            <TableCell>John</TableCell>
-            <TableCell>Doe</TableCell>
-            <TableCell>Admin</TableCell>
-            <TableCell>john@example.com</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>John</TableCell>
-            <TableCell>Doe</TableCell>
-            <TableCell>Admin</TableCell>
-            <TableCell>john@example.com</TableCell>
-          </TableRow>
+
+          {users.map((user, index) => (
+            <TableRow key={{ index }}>
+              <TableCell>{user.firstname}</TableCell>
+              <TableCell>{user.lastname}</TableCell>
+              <TableCell>Admin</TableCell>
+              <TableCell>{user.email}</TableCell>
+            </TableRow>
+          ))}
         </Table>
       </TableContainer>
       <div className='text-subheading'>Invite New User</div>
