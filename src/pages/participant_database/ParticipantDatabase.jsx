0f87585a73e15ebc070e0f0570daf16cfd786dd1
b1982384 <benwong-fodor2028@u.northwestern.edu>
2025-06-01 19:34:07 -0500

    fixed linter issues

--- src/pages/participant_database/ParticipantDatabase.jsx
+++ src/pages/participant_database/ParticipantDatabase.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+
 import { Link } from 'react-router-dom';
+
 import MenuDrawer from 'common/components/navigation/MenuDrawer';
 
 export default function ParticipantDatabase() {
